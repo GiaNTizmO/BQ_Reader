8e1224539be5eecedf5f850fce5a9cb3d50946a1
Alikhan <autocardag@gmail.com>
2020-05-04 00:44:28 +0300

    Init

---
+++ BQ_QuestsReader/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace BQ_QuestsReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BQ_QuestsReader");
+            var json = JObject.Parse(File.ReadAllText("QuestProgress.json"));
+            foreach (var quest in json["questProgress"])
+            {
+                var completed = quest["completed"].Count(user => user.Value<bool>("claimed"));
+                var questid = quest.Value<int>("questID");
+                Console.WriteLine("QuestID: " + questid + " completed: " + completed);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
